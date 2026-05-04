7b92023b7baaaba174f26916e24073ee62cb4fe6
ryo11aori-ship-it <ryo11aori@gmail.com>
2026-05-05 01:18:02 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,26 +6,53 @@
 #include"Automaton.h"
 using namespace std;
 int main(){
-const unsigned int W=800;
 const int S=100;
-float cs=(float)W/S;
+float cs=20.0f;
 Automaton aut(S);
-sf::RenderWindow window(sf::VideoMode({W,W}),"0-Dim Automaton");
+sf::RenderWindow window(sf::VideoMode({800,800}),"0-Dim Automaton");
+sf::View view=window.getDefaultView();
+view.setCenter({(S*cs)/2.0f,(S*cs)/2.0f});
+window.setView(view);
 bool running=false;
 float speed=0.1f;
 sf::Clock clock;
+bool isDragging=false;
+sf::Vector2i oldMousePos;
 while(window.isOpen()){
 while(const optional<sf::Event> event=window.pollEvent()){
 if(event->getIf<sf::Event::Closed>()){
 window.close();
+}else if(const auto* res=event->getIf<sf::Event::Resized>()){
+sf::Vector2f oldCenter=view.getCenter();
+view.setSize({(float)res->size.x,(float)res->size.y});
+view.setCenter(oldCenter);
+window.setView(view);
+}else if(const auto* wheel=event->getIf<sf::Event::MouseWheelScrolled>()){
+if(wheel->wheel==sf::Mouse::Wheel::Vertical){
+float z=(wheel->delta>0)?0.8f:1.25f;
+view.zoom(z);
+window.setView(view);
+}
 }else if(const auto* mb=event->getIf<sf::Event::MouseButtonPressed>()){
 if(mb->button==sf::Mouse::Button::Left){
-sf::Vector2f worldPos=window.mapPixelToCoords(mb->position);
-int gx=(int)round(worldPos.x/cs);
-int gy=(int)round(worldPos.y/cs);
+sf::Vector2f wPos=window.mapPixelToCoords(mb->position,view);
+int gx=(int)round(wPos.x/cs);
+int gy=(int)round(wPos.y/cs);
 if(gx>=0&&gx<S&&gy>=0&&gy<S){
 aut.toggleCell(gx,gy);
 }
+}else if(mb->button==sf::Mouse::Button::Right){
+isDragging=true;
+oldMousePos=mb->position;
+}
+}else if(const auto* mr=event->getIf<sf::Event::MouseButtonReleased>()){
+if(mr->button==sf::Mouse::Button::Right)isDragging=false;
+}else if(const auto* mm=event->getIf<sf::Event::MouseMoved>()){
+if(isDragging){
+sf::Vector2f d=window.mapPixelToCoords(oldMousePos,view)-window.mapPixelToCoords(mm->position,view);
+view.move(d);
+window.setView(view);
+oldMousePos=mm->position;
 }
 }else if(const auto* kp=event->getIf<sf::Event::KeyPressed>()){
 if(kp->code==sf::Keyboard::Key::Space)running=!running;
@@ -41,9 +68,9 @@ window.clear(sf::Color((uint8_t)20,(uint8_t)20,(uint8_t)25));
 vector<sf::Vertex> gridLines;
 for(int i=0;i<=S;i++){
 gridLines.push_back(sf::Vertex{{i*cs,0.0f},sf::Color((uint8_t)50,(uint8_t)50,(uint8_t)50)});
-gridLines.push_back(sf::Vertex{{i*cs,(float)W},sf::Color((uint8_t)50,(uint8_t)50,(uint8_t)50)});
+gridLines.push_back(sf::Vertex{{i*cs,S*cs},sf::Color((uint8_t)50,(uint8_t)50,(uint8_t)50)});
 gridLines.push_back(sf::Vertex{{0.0f,i*cs},sf::Color((uint8_t)50,(uint8_t)50,(uint8_t)50)});
-gridLines.push_back(sf::Vertex{{(float)W,i*cs},sf::Color((uint8_t)50,(uint8_t)50,(uint8_t)50)});
+gridLines.push_back(sf::Vertex{{S*cs,i*cs},sf::Color((uint8_t)50,(uint8_t)50,(uint8_t)50)});
 }
 window.draw(gridLines.data(),gridLines.size(),sf::PrimitiveType::Lines);
 const auto& g=aut.getGrid();
@@ -57,8 +84,9 @@ window.draw(line,2,sf::PrimitiveType::Lines);
 for(int y=0;y<S;y++){
 for(int x=0;x<S;x++){
 if(g[y][x].isAlive){
-sf::CircleShape dot(3.0f);
-dot.setOrigin({1.5f,1.5f});
+float r=cs*0.15f;
+sf::CircleShape dot(r);
+dot.setOrigin({r,r});
 dot.setPosition({x*cs,y*cs});
 dot.setFillColor(sf::Color::Cyan);
 window.draw(dot);
